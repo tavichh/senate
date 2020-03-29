98e25404642d59841c795ba805ac88467c69f2a5
tavichh <tavichh@rustysheep.com>
2020-03-28 22:15:35 -0400

    Added the basis of the plot voting system

--- Configuration.cs
+++ Configuration.cs
@@ -5,6 +5,9 @@
         public static readonly string Token = Hidden.token;
         public static readonly ulong Senator = Hidden.senator;
         public static readonly string Plot = "https://www.ancient.eu/img/c/p/1200x627/4532.jpg?v=1569514716";
+
+        public static readonly string PlotVote =
+            "https://someinterestingfacts.net/wp-content/uploads/2013/01/Roman-Senate1.jpg";
         public static readonly string PlotSuccess = "https://cropper.watch.aetnd.com/cdn.watch.aetnd.com/sites/2/2018/05/GettyImages-587489068.jpg";
         public static readonly string PlotFailure = "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Decapitaci%C3%B3n_de_San_Pablo_-_Simonet_-_1887.jpg/1200px-Decapitaci%C3%B3n_de_San_Pablo_-_Simonet_-_1887.jpg";
 
--- Modules/Plotting.cs
+++ Modules/Plotting.cs
@@ -13,6 +13,7 @@ namespace Senate.Modules
         private List<string> plotters = new List<string>();
         private List<IGuildUser> senate = new List<IGuildUser>();
         private string plotterList, senatorList;
+        public int Aye, Nay;
 
         [Command("plot join")]
         public async Task Plot()
@@ -26,6 +27,7 @@ namespace Senate.Modules
                     senate.Add(member);
                     senatorList += member.Username + ",";
                 }
+
             foreach (SocketRole role in ((SocketGuildUser) Context.Message.Author).Roles)
             {
                 if (role.Id == senator)
@@ -33,7 +35,7 @@ namespace Senate.Modules
                     plotters.Add(Context.User.Username);
                     foreach (var member in plotters)
                         plotterList += member + ", ";
-                    
+
                     power = plotters.Count;
                     Console.WriteLine($"{Context.Message.Author.Username} has joined a plot!");
                     Console.WriteLine($"There are currently {power} plotters, and {senate.Count} senators!");
@@ -55,7 +57,29 @@ namespace Senate.Modules
         [Command("plot start")]
         public async Task StartPlot()
         {
-            Console.WriteLine($"{Context.User.Username} is trying to execute the plot to assassinate the Caesar!");
+            Console.WriteLine($"{Context.User.Username} started a vote with {plotters.Count} plotters!");
+            var plotVoteWindow = new EmbedBuilder()
+            {
+                Title = "Where does your loyalty lie?",
+                Description =
+                    "The time has come to act. The Caesar must die. Type plot yes/no to choose if now is the time to move forward with the plot.",
+                ImageUrl = Configuration.PlotVote,
+            };
+            await ReplyAsync(embed: plotVoteWindow.Build());
+        }
+
+        [Command("plot yes")]
+        public async Task PlotYes()
+        {
+            Aye++;
+            await ReplyAsync($"Aye's: {Aye} | Nay's: {Nay}");
+        }
+        
+        [Command("plot no")]
+        public async Task PlotNo()
+        {
+            Nay++;
+            await ReplyAsync($"Aye's: {Aye} | Nay's: {Nay}");
         }
     }
 }
