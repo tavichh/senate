df04decb66ae7976e8bc5e5ca037934f11387979
tavichh <tavichh@rustysheep.com>
2020-03-28 20:02:20 -0400

    Added Project Files

---
+++ Modules/Plot.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Discord.WebSocket;
+
+namespace Senate.Modules
+{
+    public class Plot
+    {
+        public List<string> members = new List<string>();
+        public int PlotPower;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+// ReSharper disable All
+
+namespace Senate
+{
+    class Program
+    {
+        static void Main(string[] args) => new Program()
+            .RunBotAsync()
+            .GetAwaiter()
+            .GetResult();
+
+        private DiscordSocketClient client;
+        private CommandService commands;
+        private IServiceProvider services;
+
+        private async Task RunBotAsync()
+        {
+            client = new DiscordSocketClient();
+            commands = new CommandService();
+            services = new ServiceCollection()
+                .AddSingleton(client)
+                .AddSingleton(commands)
+                .BuildServiceProvider();
+            var token = Token.id;
+            client.Log += Log;
+            await RegisterCommandsAsync();
+            await client.LoginAsync(TokenType.Bot, token);
+            await client.StartAsync();
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+
+        private async Task RegisterCommandsAsync()
+        {
+            client.MessageReceived += HandleMessages;
+            await commands.AddModulesAsync(Assembly.GetEntryAssembly(), services);
+        }
+
+        private async Task HandleMessages(SocketMessage arg)
+        {
+            var message = arg as SocketUserMessage;
+            var context = new CommandContext(client, message);
+            int argpos = 0;
+
+            if (message.Author.Username == "tavichh")
+            {
+                await message.Channel.SendMessageAsync("Hail Caesar! May he reign for a thousand years!");
+            }
+
+            if (message.HasStringPrefix("!", ref argpos))
+            {
+                var result = await commands.ExecuteAsync(context, argpos, services);
+                if (!result.IsSuccess) Console.WriteLine(result.ErrorReason);
+            }
+        }
+    }
+}
